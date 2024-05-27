01a79a336a2ddbcfa7af91b807de95b43212a114
AtakishiAliyev <atakishieliyev@gmail.com>
2024-05-27 18:51:57 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -40,14 +40,14 @@ function App() {
         min-field-of-view="25deg"
         max-field-of-view="45deg"
         interpolation-decay="200"
-        min-camera-orbit="auto auto 50%"
+        min-camera-orbit="auto auto 40%"
       >
         <button
           onClick={handleButton}
           className="view-button"
           slot="hotspot-0"
-          data-position="-1.296 0.432 0.446"
-          data-normal="-0.030 0.999 -0.002"
+          data-position="-0.09916310941123264 0.7996048093193944 -0.5609200215894207"
+          data-normal="-0.00037559329829801836 0.34042594420144645 0.940271256310767"
         >
           Another Point 0
         </button>
@@ -68,6 +68,15 @@ function App() {
           data-normal="0.1 0.9 0.1"
         >
           Another Point 2
+        </button>
+        <button
+          onClick={handleButton}
+          className="view-button"
+          slot="hotspot-3"
+          data-position="0.8188320487793729 0.4295735849775249 0.05403621774045279"
+          data-normal="0 0.8839126703352922 0.4676519979864655"
+        >
+          Another Point 3
         </button>
       </model-viewer>
     </main>
